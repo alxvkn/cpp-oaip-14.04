b1ae268a53ef2361818928f4f0bed1289a2626b2
alxvkn <64454213+alxvkn@users.noreply.github.com>
2025-04-24 05:27:00 +0300

    use wxChoice instead of wxComboBox

--- 3.cpp
+++ 3.cpp
@@ -32,11 +32,13 @@ public:
             choices.Add(c.first);
         }
 
-        combobox_left = new wxComboBox(panel, wxID_ANY, "RUB", wxDefaultPosition, wxSize(100, -1),
-                                    choices, wxCB_READONLY);
+        choice_left = new wxChoice(panel, wxID_ANY, wxDefaultPosition, wxSize(100, -1),
+                                    choices);
+        choice_left->SetStringSelection("RUB");
 
-        combobox_right = new wxComboBox(panel, wxID_ANY, "BYN", wxDefaultPosition, wxSize(100, -1),
-                                    choices, wxCB_READONLY);
+        choice_right = new wxChoice(panel, wxID_ANY, wxDefaultPosition, wxSize(100, -1),
+                                    choices);
+        choice_right->SetStringSelection("BYN");
 
         custom_rate_checkbox = new wxCheckBox(panel, wxID_ANY, wxT("Свой курс"));
         custom_rate_input = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition, wxSize(150, -1));
@@ -45,8 +47,8 @@ public:
 
         grid->Add(input_left, 0, wxALL, 10);
         grid->Add(input_right, 0, wxALL, 10);
-        grid->Add(combobox_left, 0, wxBOTTOM | wxLEFT | wxRIGHT, 10);
-        grid->Add(combobox_right, 0, wxBOTTOM | wxLEFT | wxRIGHT, 10);
+        grid->Add(choice_left, 0, wxBOTTOM | wxLEFT | wxRIGHT, 10);
+        grid->Add(choice_right, 0, wxBOTTOM | wxLEFT | wxRIGHT, 10);
         grid->Add(0, 0);
         grid->Add(custom_rate_checkbox, 1, wxTOP | wxLEFT | wxRIGHT, 10);
         grid->Add(0, 0);
@@ -57,13 +59,13 @@ public:
 
         input_left->Bind(wxEVT_TEXT, [this](wxCommandEvent& event) { OnLeftInputChange(event); });
         input_right->Bind(wxEVT_TEXT, [this](wxCommandEvent& event) { OnRightInputChange(event); });
-        combobox_left->Bind(wxEVT_COMBOBOX, [this](wxCommandEvent& event) { OnLeftChoice(event); });
-        combobox_right->Bind(wxEVT_COMBOBOX, [this](wxCommandEvent& event) { OnRightChoice(event); });
+        choice_left->Bind(wxEVT_COMBOBOX, [this](wxCommandEvent& event) { OnLeftChoice(event); });
+        choice_right->Bind(wxEVT_COMBOBOX, [this](wxCommandEvent& event) { OnRightChoice(event); });
         custom_rate_checkbox->Bind(wxEVT_CHECKBOX, [this](wxCommandEvent& event) { OnCustomRateCheckboxChange(event); });
         custom_rate_input->Bind(wxEVT_TEXT, [this](wxCommandEvent& event) { OnCustomRateInputChange(event); });
 
-        rate_left = rates_provider->rates().at(combobox_left->GetStringSelection().ToStdString());
-        rate_right = rates_provider->rates().at(combobox_right->GetStringSelection().ToStdString());
+        rate_left = rates_provider->rates().at(choice_left->GetStringSelection().ToStdString());
+        rate_right = rates_provider->rates().at(choice_right->GetStringSelection().ToStdString());
 
         update();
     }
@@ -72,8 +74,8 @@ private:
     wxTextCtrl* input_left;
     wxTextCtrl* input_right;
 
-    wxComboBox* combobox_left;
-    wxComboBox* combobox_right;
+    wxChoice* choice_left;
+    wxChoice* choice_right;
 
     wxCheckBox* custom_rate_checkbox;
     bool custom_rate_state = false;
@@ -89,8 +91,8 @@ private:
     void OnCustomRateCheckboxChange(wxCommandEvent& event) {
         custom_rate_state = custom_rate_checkbox->GetValue();
 
-        combobox_left->Enable(!custom_rate_state);
-        combobox_right->Enable(!custom_rate_state);
+        choice_left->Enable(!custom_rate_state);
+        choice_right->Enable(!custom_rate_state);
 
         if (custom_rate_state)
             custom_rate_input->Show();
@@ -110,14 +112,14 @@ private:
     }
 
     void OnLeftChoice(wxCommandEvent& event) {
-        rate_left = rates_provider->rates().at(combobox_left->GetStringSelection().ToStdString());
+        rate_left = rates_provider->rates().at(choice_left->GetStringSelection().ToStdString());
 
         value_right = value_left * get_rate();
         update();
     }
 
     void OnRightChoice(wxCommandEvent& event) {
-        rate_right = rates_provider->rates().at(combobox_right->GetStringSelection().ToStdString());
+        rate_right = rates_provider->rates().at(choice_right->GetStringSelection().ToStdString());
 
         value_right = value_left * get_rate();
         update();
